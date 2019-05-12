51ac314f16b4e6265d3b658bf6273e501b439c1f
Sean M <srmcquay@gmail.com>
2019-05-12 18:00:00 -0500

    Added check for branch naming.

---
+++ scripts/branchCheck.js
@@ -0,0 +1,24 @@
+const branchName = require('current-git-branch');
+
+const MAIN_BRANCH_NAMES = [
+    'develop',
+    'master'
+];
+
+const BRANCH_PREFIX_NAMES = [
+    'bugfix',
+    'feature',
+    'hotfix',
+    'release',
+    'test'
+];
+
+(function checkBranchNaming() {
+    const branchRegex = new RegExp(`^(${MAIN_BRANCH_NAMES.join('|')})|((${BRANCH_PREFIX_NAMES.join('|')})/(([a-z]+((-[a-z]+)+)?)|(\\d+\\.\\d+\\.\\d+)))$`);
+
+    const currentBranchName = branchName();
+
+    if (!branchRegex.test(currentBranchName)) {
+        throw new Error(`Branch naming convention must adhere to ${branchRegex}`);
+    }
+}());
