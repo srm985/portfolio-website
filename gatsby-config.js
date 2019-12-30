3e00aa3ec1fa85280cd90e81eb2c59d4cfadd2d1
Sean M <srmcquay@gmail.com>
2019-12-30 14:32:40 -0600

    Removed favicon linking.

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,6 @@ module.exports = {
             options: {
                 background_color: '#663399',
                 display: 'minimal-ui',
-                icon: 'src/images/gatsby-icon.png',
                 // This path is relative to the root of the site.
                 name: 'gatsby-starter-default',
                 short_name: 'starter',
