470b482f2dd45107677e991ca6159e2161b339d8
Sean McQuay <srmcquay@gmail.com>
2020-01-08 16:25:11 -0600

    Updated Storybook stories.

--- src/components/HeaderComponent/stories.js
+++ src/components/HeaderComponent/stories.js
@@ -7,28 +7,54 @@ import HeaderComponent from './index';
 
 const stories = storiesOf(HeaderComponent.displayName, module);
 
-stories.add('default', () => (
-    <div>
-        <HeaderComponent />
-        <div style={{
-            backgroundColor: '#333',
-            height: '500px'
-        }}
-        />
-        <div style={{
-            backgroundColor: 'white',
-            height: '500px'
-        }}
-        />
-        <div style={{
-            backgroundColor: 'blue',
-            height: '500px'
-        }}
-        />
-        <div style={{
-            backgroundColor: 'orange',
-            height: '500px'
-        }}
-        />
-    </div>
-));
+stories.add('default', () => {
+    const MOCKED_QUERY = {
+        edges: [
+            {
+                node: {
+                    childMarkdownRemark: {
+                        frontmatter: {
+                            headerLinks: [
+                                {
+                                    pageName: 'DEMO_1',
+                                    pageURL: ''
+                                },
+                                {
+                                    pageName: 'DEMO_2',
+                                    pageURL: ''
+                                }
+                            ],
+                            logoCopy: 'Demo Header'
+                        }
+                    }
+                }
+            }
+        ]
+    };
+
+    return (
+        <div>
+            <HeaderComponent data={MOCKED_QUERY} />
+            <div style={{
+                backgroundColor: '#333',
+                height: '500px'
+            }}
+            />
+            <div style={{
+                backgroundColor: 'white',
+                height: '500px'
+            }}
+            />
+            <div style={{
+                backgroundColor: 'blue',
+                height: '500px'
+            }}
+            />
+            <div style={{
+                backgroundColor: 'orange',
+                height: '500px'
+            }}
+            />
+        </div>
+    );
+});
