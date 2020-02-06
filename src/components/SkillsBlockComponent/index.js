b9aeee3284a8a621d3f9c943de3c269c1b0952d4
Sean M <srmcquay@gmail.com>
2020-02-05 21:25:53 -0600

    Added skill search functionality.

--- src/components/SkillsBlockComponent/index.js
+++ src/components/SkillsBlockComponent/index.js
@@ -1,63 +1,120 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import Input from '../InputComponent';
 import SkillPill from '../SkillPillComponent';
+
+import {
+    INPUT_TYPE_SEARCH
+} from '../InputComponent/config';
 
 import './styles.scss';
 
-const SkillsBlockComponent = (props) => {
-    const {
-        skillsSectionList
-    } = props;
+class SkillsBlockComponent extends React.Component {
+    constructor(props) {
+        super(props);
 
-    const {
-        displayName
-    } = SkillsBlockComponent;
+        this.state = {
+            skillSearchKeyword: ''
+        };
+    }
 
-    const renderSkillPill = (skillDetails = {}) => {
+    handleSkillSearchKeywordChange=(event) => {
+        const {
+            target: {
+                value: skillSearchKeyword
+            }
+        } = event;
+
+        this.setState({
+            skillSearchKeyword
+        });
+    }
+
+    renderSkillPill = (skillDetails = {}) => {
+        const {
+            state: {
+                skillSearchKeyword
+            }
+        } = this;
+
         const {
             icon = '',
             label = ''
         } = skillDetails;
 
-        return (
-            <SkillPill
-                icon={icon}
-                key={label}
-                label={label}
-            />
-        );
-    };
+        const doesSkillFilterMatch = label.toLowerCase().includes(skillSearchKeyword.toLowerCase());
 
-    const renderSkillsBlock = (skillsBlockDetails = {}) => {
+        return (
+            doesSkillFilterMatch && (
+                <SkillPill
+                    icon={icon}
+                    key={label}
+                    label={label}
+                />
+            )
+        );
+    }
+
+    renderSkillsBlock = (skillsBlockDetails = {}) => {
         const {
             sectionLabel = '',
             skillsList = []
         } = skillsBlockDetails;
+
+        const {
+            displayName
+        } = SkillsBlockComponent;
+
+        const generatedSkillPills = skillsList.map(this.renderSkillPill).filter((skill) => !!skill);
 
         return (
             <div
                 className={`${displayName}__skills-section-block`}
                 key={sectionLabel}
             >
-                <h3 className={`${displayName}__skills-section-label`}>{sectionLabel}</h3>
+                <h4 className={`${displayName}__skills-section-label`}>{sectionLabel}</h4>
                 <div className={`${displayName}__skills-section`}>
                     {
-                        skillsList.map(renderSkillPill)
+                        generatedSkillPills.length ? (
+                            generatedSkillPills
+                        ) : (
+                            <p>Sorry, no relevant skills found...</p>
+                        )
                     }
                 </div>
             </div>
         );
-    };
+    }
 
-    return (
-        <div className={displayName}>
-            {
-                skillsSectionList.map(renderSkillsBlock)
+    render() {
+        const {
+            props: {
+                skillsSectionList
             }
-        </div>
-    );
-};
+        } = this;
+
+        const {
+            displayName
+        } = SkillsBlockComponent;
+
+        return (
+            <div className={displayName}>
+                <h3 className={`${displayName}__title`}>{'Do I have the skills you need?'}</h3>
+                <Input
+                    handleChange={this.handleSkillSearchKeywordChange}
+                    label={'Find a skill!'}
+                    name={'skillSearchbar'}
+                    placeholder={'Search'}
+                    type={INPUT_TYPE_SEARCH}
+                />
+                {
+                    skillsSectionList.map(this.renderSkillsBlock)
+                }
+            </div>
+        );
+    }
+}
 
 SkillsBlockComponent.displayName = 'SkillsBlockComponent';
 
