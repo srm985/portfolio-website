4506997a0ec295d7d8dfa229f3273764e2e77ec4
Sean M <srmcquay@gmail.com>
2020-06-20 21:34:06 -0500

    Working Save

--- src/connected/ProjectTemplateConnected.js
+++ src/connected/ProjectTemplateConnected.js
@@ -107,6 +107,7 @@ export const query = graphql`
                                 publicURL
                             }
                             returnButtonLink
+                            returnButtonScreenReaderLabel
                             sourceCodeCTA
                         }
                     }
