a5228e57e65ce8934712ae4c0d11d1bfa4b1aba0
Sean M <srmcquay@gmail.com>
2020-06-27 00:33:20 -0500

    Added functionality to home buttons.

--- src/utils/bodyScrolling.js
+++ src/utils/bodyScrolling.js
@@ -1,13 +1,15 @@
-const bodyElement = document.querySelector('body');
-
 const SCROLL_PREVENTION_CLASS = 'scroll-disabled';
 
 const bodyScrolling = {
     disable: () => {
+        const bodyElement = document.querySelector('body');
+
         bodyElement.classList.add(SCROLL_PREVENTION_CLASS);
     },
 
     enable: () => {
+        const bodyElement = document.querySelector('body');
+
         bodyElement.classList.remove(SCROLL_PREVENTION_CLASS);
     }
 };
